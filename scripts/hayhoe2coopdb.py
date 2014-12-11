76c13e1a7c200f7454db7d5fcec904bd07d5d936
akrherz <akrherz@iastate.edu>
2014-12-11 15:54:07 -0600

    major cleanup and properly account masked values

--- scripts/hayhoe2coopdb.py
+++ scripts/hayhoe2coopdb.py
@@ -1,26 +1,27 @@
-'''
+"""
 Copy the Hayhoe data into my local database
 
    model   | scenario 
 -----------+----------
- echam5    | a1b x
- echo      | a1b x
- cnrm      | a1b x
- cgcm3_t47 | a1b x
- miroc_hi  | a1b x
+ echam5    | a1b xf
+ echo      | a1b xf
+ cnrm      | a1b xf
+ cgcm3_t47 | a1b xf
+ miroc_hi  | a1b xf
  hadgem    | a1b x
  cgcm3_t63 | a1b x
- hadcm3    | a1b x
- pcm       | a1b x
- giss_aom  | a1b x
+ hadcm3    | a1b 
+ pcm       | a1b 
+ giss_aom  | a1b 
 
-'''
+"""
 import netCDF4
 import datetime
 import numpy as np
 import psycopg2
-from pyiem.datatypes import temperature
+from pyiem.datatypes import temperature, distance
 import sys
+from pyiem.network import Table as NetworkTable
 
 PGCONN = psycopg2.connect(database='coop')
 cursor = PGCONN.cursor()
@@ -28,24 +29,53 @@ cursor = PGCONN.cursor()
 model = sys.argv[1]
 scenario = sys.argv[2]
 
-pr_nc = netCDF4.Dataset('/tera13/akrherz/hayhoe/%s.%s.pr.NAm.grid.1960.2099.nc' % (model, scenario))
-tasmax_nc = netCDF4.Dataset('/tera13/akrherz/hayhoe/%s.%s.tmax.NAm.grid.1960.2099.nc' % (model, scenario))
-tasmin_nc = netCDF4.Dataset('/tera13/akrherz/hayhoe/%s.%s.tmin.NAm.grid.1960.2099.nc' % (model, scenario))
+nt = NetworkTable("CSCAP")
+done = []
+
+BASE = "/tera13/akrherz/hayhoe"
+
+pr_nc = netCDF4.Dataset('%s/%s.%s.pr.NAm.grid.1960.2099.nc' % (BASE,
+                                                model, scenario))
+tasmax_nc = netCDF4.Dataset('%s/%s.%s.tmax.NAm.grid.1960.2099.nc' % (BASE,
+                                                model, scenario))
+tasmin_nc = netCDF4.Dataset('%s/%s.%s.tmin.NAm.grid.1960.2099.nc' % (BASE,
+                                                model, scenario))
 
 tokens = (pr_nc.variables['time'].units).replace("days since ", "").split("-")
-basets = datetime.datetime( int(tokens[0]), int(tokens[1]), int(tokens[2]) )
+basets = datetime.date( int(tokens[0]), int(tokens[1]), int(tokens[2]) )
 
 tmdata = pr_nc.variables['time'][:]
-print basets, np.shape(tmdata)[0] / 365.0
+if basets != datetime.date(1959,12,31):
+    print('FAILURE, basets %s is not 31 Dec 1959' % (basets,))
+    sys.exit()
+if (np.shape(tmdata)[0] / 365.0) != 140:
+    print('FAILURE, %s years found, not exactly 140' % (
+                                    np.shape(tmdata)[0] / 365.0,))
+    sys.exit()
 
 # Files have degrees east 0-360, so 190 is -170 , 200 is -160
 lons = pr_nc.variables['lon'][:] - 360.0
 lats = pr_nc.variables['lat'][:]
 
-def do(lon, lat, station):
 
+def fix(val):
+    """ Convert a value into something reasonable """
+    if np.ma.is_masked(val):
+        return None
+    return float(val)
+
+def insert(station, now, high, low, precip):
+    """ Add a database entry """
+    cursor.execute("""
+    INSERT into hayhoe_daily(model, scenario, station, day, high, low, precip)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    """, (model, scenario, station, now, high, low, precip))
+
+def do(lon, lat, station):
+    """ Process this station and geography """
     idx = np.digitize([lon,], lons)[0]
     jdx = np.digitize([lat,], lats)[0]
+    print("--> Processing %s i:%s j:%s" % (station, idx, jdx))
 
     pdata = pr_nc.variables['pr'][:,jdx,idx]
     xdata = tasmax_nc.variables['tmax'][:,jdx,idx]
@@ -53,38 +83,34 @@ def do(lon, lat, station):
 
     highs = temperature(xdata, 'C').value('F')
     lows = temperature(ndata, 'C').value('F')
-    precips = pdata / 24.5
+    precips = distance(pdata, 'MM').value('IN')
 
     now = basets
-    for k, val in enumerate(tmdata):
+    high = low = precip = None
+    for k, _ in enumerate(tmdata):
         now += datetime.timedelta(days=1)
         if now.month == 2 and now.day == 29:
             # Insert missing data
-            cursor.execute("""
-        INSERT into hayhoe_daily(model, scenario, station, day, high, low,
-        precip) values (%s, %s, %s, %s, %s, %s, %s)
-        """, (model, scenario, station, now, high, low, precip))            
+            insert(station, now, high, low, precip)           
             now += datetime.timedelta(days=1)
-        high = float(highs[k])
-        low = float(lows[k])
-        if low > high:
+        high = fix(highs[k])
+        low = fix(lows[k])
+        if low is not None and high is not None and low > high:
             # Swap, sigh
-            print '%s %s high: %.1f low: %.1f was swapped' % (
-                        now.strftime("%m-%d-%Y"), station, high, low)
+            print('%s %s high: %.1f low: %.1f was swapped' % (
+                        now.strftime("%m-%d-%Y"), station, high, low))
             high2 = high
             high = low
             low = high2
-        precip = float(precips[k])
-        cursor.execute("""
-        INSERT into hayhoe_daily(model, scenario, station, day, high, low,
-        precip) values (%s, %s, %s, %s, %s, %s, %s)
-        """, (model, scenario, station, now, high, low, precip))
-            
+        precip = fix(precips[k])
+        insert(station, now, high, low, precip)
 
-for line in open('cscap_sites.csv'):
-    lon, lat, station = line.strip().split(",")
-    print 'Processing %s' % (station,)
-    do(float(lon), float(lat), station)
+for sid in nt.sts.keys():
+    climatesite = nt.sts[sid]['climate_site']
+    if climatesite in done:
+        continue
+    done.append(climatesite)
+    do(nt.sts[sid]['lon'], nt.sts[sid]['lat'], climatesite)
     
 cursor.close()
 PGCONN.commit()
