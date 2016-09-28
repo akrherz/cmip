0b19b327503b3c212672c51ede5eb8aca0b6581a
akrherz <akrherz@iastate.edu>
2016-09-28 11:01:11 -0500

    generalize

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -14,19 +14,20 @@ models = ['miroc_hi',
           'cnrm',
           'pcm']
 
-# models = ['cgcm3_t63', 'pcm', 'hadcm3', 'echo', 'cnrm']
+models = ['cgcm3_t47', 'cgcm3_t63', 'echam5', 'hadgem', 'echo', 'cnrm']
 
 
-for sid in ['BDMARI', 'BDCLOV', 'BDLOSL', 'BDTUCU', 'BDRENO', 'BDFALL']:
-    # climatesite = nt.sts[sid]['climate_site']
+# for sid in ['BDMARI', 'BDCLOV', 'BDLOSL', 'BDTUCU', 'BDRENO', 'BDFALL']:
+for sid in nt.sts.keys():
+    climatesite = nt.sts[sid]['climate_site']
     for model in models:
         print 'Running %s %s %s' % (model, 'bogus', sid)
         uri = ("http://iem.local/cgi-bin/request/coop.py?network=%sCLIMATE&"
                "station[]=%s&year1=1961&month1=1&day1=1&"
                "year2=2099&month2=1&day2=1&vars[]=daycent&"
                "hayhoe_scenario=a1b&hayhoe_model=%s&"
-               "what=view&delim=comma&gis=no&scenario_year=2013"
-               ) % ('IA', sid, model)
+               "what=view&delim=comma&gis=no"
+               ) % ('IA', climatesite, model)
 
         data = urllib2.urlopen(uri).read()
 
